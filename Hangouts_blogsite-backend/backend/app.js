3996d6b73f7dd2193aed2fa4a00a35cc59ad5b53
reena-joy-kota <108350528+reena-joy-kota@users.noreply.github.com>
2023-07-17 12:17:04 +0530

    Add files via upload

---
+++ Hangouts_blogsite-backend/backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose.connect("mongodb+srv://reenajoykota20:<reeNA@21>@cluster0.7hocar7.mongodb.net/?retryWrites=true&w=majority")
+.then(() => app.listen(5000),console.log("entered"))
+.then(() =>
+  console.log("Connected TO Database and Listening TO Localhost 5000")
+)
+.catch((err) => console.log(err));
